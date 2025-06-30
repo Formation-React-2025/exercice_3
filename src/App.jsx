73c446bd9d2a4658fd25d40852c4bfb856d5dbf2
ThibaultCGI <thibault.bondetti@cgi.com>
2025-07-01 00:42:08 +0200

    Correction
    * init
    
    * correction
    
    * update README.md
    
    * update package.json
    
    * fix after merge
    
    * update README.md
    
    * correction
    
    * init
    
    * init
    
    * correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Exercice2Page from './features/exercice-2/pages/Exercice2Page';
+import Exercice3 from './features/exercice-3/pages/Exercice3';
 
 const App = () => (
   <main>
-    <Exercice2Page
-      title="Exercice 2"
-    >
-      <span>Hello world !</span>
-    </Exercice2Page>
+    <Exercice3 />
   </main>
 );
 
